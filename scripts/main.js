471f1df18306c07c3b9b2648c57de14ffa061437
Ishaan Ray <cipher.shadow.1972k6@gmail.com>
2025-03-23 22:42:09 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,22 +21,27 @@ function startListening() {
 
 function handleCommand(command) {
   if (command.includes("youtube")) {
+    speak('Surely! Opening Youtube.');
     window.open("https://youtube.com", "_blank");
   } else if (command.includes("time")) {
     const now = new Date();
     const time = now.toLocaleTimeString();
-    speak("The time is " + time);
+    speak("The time in your region is " + time);
   } else if (command.includes("compiler")) {
+    speak('Ahhh! Be ready!!! \n The INFINITY COMPILER HUB made by Ishaan Ray is coming up in your screen, go on with numerous compilers; Traveller!');
     window.open("https://infinitycompilerhub.netlify.app/", "_blank");
   } else if (command.includes("github")) {
+    speak('Surely! Opening Github.');
       if (command.includes("profile")){
         window.open("https://github.com/Cipher-Shadow-IR/", "_blank");
       } else {
         window.open("https://github.com/", "_blank");
       }
   } else if (command.includes("mail")) {
+    speak('Gotta Check Inbox? \n here you go!.');
     window.open("https://mail.google.com/mail/", "_blank");
   } else if (command.includes("note")) {
+    speak('Want to make a Note? \n here you go! make sure to save the note lol!');
     window.open("https://www.rapidtables.com/tools/notepad.html", "_blank");
   } else {
     speak("Sorry, I didn't get that.");
